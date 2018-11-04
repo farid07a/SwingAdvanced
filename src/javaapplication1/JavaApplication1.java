5aba43b861094fe557965fcf44a752413687d5cd
farid07a <faridmaster02@gmail.com>
2018-11-04 22:30:59 +0100

    first commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author horizon
+ */
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+class ammar{
+static int i,j; 
+
+    public ammar(int i,int j) {
+        this.i=i;
+        this.j=j;
+    }
+ 
+    
+}
